810209e549d25dbff1c0f4610f94b28df32269e8
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-03 12:03:55 +0900

    navBar修正した

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
+import rocketImage from '../assets/rocket.gif';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="w-full container mx-auto justify-between items-center p-5">
-      <h3 className="text-3xl font-bold">Redux with TypeScript Shop Cart</h3>
+        <div className='flex gap-3 items-center justify-center mx-auto'>
+            <h3 className="text-3xl font-bold">SHOP CART PROJECT</h3>
+            <p className='text-lg font-bold'>using Redux with TypeScript</p>
+            <img src={rocketImage} alt="logo" width={80} height={80}/>
+        </div>
       <div className="relative flex justify-between">
-        <p>logo</p>
         <div className="absolute -top-2.5 -right-2.5 w-10 h-10 rounded-full bg-gray-800 flex items-center justify-center">
             <p className="text-white mb-0 text-xl">0</p>
         </div>
