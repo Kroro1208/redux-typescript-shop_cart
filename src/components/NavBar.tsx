d3be30343fe526bfd553bfe675531cbb3aaf5ce5
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-03 12:26:15 +0900

    NavBarのスタイル変更した

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,28 @@
+import { RootState } from '../app/store';
 import rocketImage from '../assets/rocket.gif';
+import { useSelector } from 'react-redux';
+import shopCartImage from '../assets/cart.gif';
 
 const NavBar: React.FC = () => {
-  return (
-    <nav className="w-full container mx-auto justify-between items-center p-5">
-        <div className='flex gap-3 items-center justify-center mx-auto'>
-            <h3 className="text-3xl font-bold">SHOP CART PROJECT</h3>
-            <p className='text-lg font-bold'>using Redux with TypeScript</p>
-            <img src={rocketImage} alt="logo" width={80} height={80}/>
-        </div>
-      <div className="relative flex justify-between">
-        <div className="absolute -top-2.5 -right-2.5 w-10 h-10 rounded-full bg-gray-800 flex items-center justify-center">
-            <p className="text-white mb-0 text-xl">0</p>
-        </div>
-      </div>
-    </nav>
-  )
+    const amount = useSelector((state: RootState) => state.cart.amount);
+    console.log(amount);
+
+    return (
+        <nav className="w-full container mx-auto flex justify-between items-center p-5">
+            <div className='flex gap-3 items-center'>
+                <h3 className="text-3xl font-bold">SHOP CART PROJECT</h3>
+                <p className='text-lg font-bold'>using Redux with TypeScript</p>
+                <img src={rocketImage} alt="logo" width={80} height={80}/>
+            </div>
+            <div className="flex items-center relative">
+                <img src={shopCartImage} alt="cart" width={50} height={50} />
+                <div className="absolute -top-3 -right-3 w-8 h-8 rounded-full
+                bg-gray-800 flex items-center justify-center transform translate-x-2 translate-y-2">
+                    <p className="text-white mb-0 text-lg">{amount}</p>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default NavBar
+export default NavBar;
