914782b3dc611fc5617377446a8017c46ab7d270
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-03 13:47:33 +0900

    modalSlice作成した

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 import NavBar from './components/NavBar.tsx'
+import { CartContainer } from './components/CartContainer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <NavBar />
+      <CartContainer />
       <App />
     </Provider>
   </React.StrictMode>,
